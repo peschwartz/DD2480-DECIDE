7dbe04ecfc8fe63d2ce059db03f8a7c99cb26ff9
phoebe <phoebes@kth.se>
2025-01-24 14:15:47 +0100

    added more testing for lic3. solves #14

--- test/main.py
+++ test/main.py
@@ -1,14 +1,8 @@
 import unittest
-from testcmv import TestCMV
-from testfuv import TestFUV
 
 if __name__ == '__main__':
-    test_suite = unittest.TestLoader().loadTestsFromTestCase(TestCMV)
+    # make sure all test files start with test
+    test_suite = unittest.TestLoader().discover('test', pattern='test*.py')
     
     unittest.TextTestRunner(verbosity=2).run(test_suite)
 
-    test_suite2 = unittest.TestLoader().loadTestsFromTestCase(TestFUV)
-    
-    unittest.TextTestRunner(verbosity=2).run(test_suite2)
-
-    
