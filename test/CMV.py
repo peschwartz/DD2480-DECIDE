979dd31f8aa11f428c99d25161ce1cae1210c84b
Samuel Söderberg <samuelsoderberg99@gmail.com>
2025-01-24 10:39:02 +0100

    CMV testing framework established #27

--- test/CMV.py
+++ test/CMV.py
@@ -1,31 +1,50 @@
 import unittest
 from cmv import *
-from VARIABLES import *
+from GLOBAL_VARS import *
 
 class TestCMV(unittest.TestCase):
 
     def test_0(self):
-        self.assertEqual(cmv_0(.LENGTH1), False)
+        self.assertEqual(lic_0(PARAMETERS.LENGTH1), False)
 
-    def test_1(self, RADIUS1: float):
-        self.assertEqual(cmv_1(RADIUS1), False)
+    def test_1(self):
+        self.assertEqual(lic_1(PARAMETERS.RADIUS1), False)
 
-    def test_2(self, EPSILON: float):
-        self.assertEqual(cmv_2(EPSILON), False)
+    def test_2(self):
+        self.assertEqual(lic_2(PARAMETERS.EPSILON), False)
 
-    def test_3(self, AREA1: float):
-        self.assertEqual(cmv_3(AREA1), False)
+    def test_3(self):
+        self.assertEqual(lic_3(PARAMETERS.AREA1), False)
 
-    def test_4(self, Q_PTS: int, QUADS: int):
-        self.assertEqual(cmv_4(Q_PTS, QUADS), False)
+    def test_4(self):
+        self.assertEqual(lic_4(PARAMETERS.Q_PTS, PARAMETERS.QUADS), False)
 
-    def test_5(self, DIST: float):
-        self.assertEqual(cmv_5(DIST), False)
+    def test_5(self):
+        self.assertEqual(lic_5(PARAMETERS.DIST), False)
 
-    def test_6(self, N_PTS: int, K_PTS: int):
-        self.assertEqual(cmv_6(N_PTS, K_PTS), False)
+    def test_6(self):
+        self.assertEqual(lic_6(PARAMETERS.N_PTS, PARAMETERS.K_PTS), False)
 
-    def test_7(self, K_PTS: int):
-        self.assertEqual(cmv_7(K_PTS), False)
+    def test_7(self):
+        self.assertEqual(lic_7(PARAMETERS.K_PTS), False)
 
-    
+    def test_8(self):
+        self.assertEqual(lic_8(PARAMETERS.A_PTS, PARAMETERS.B_PTS), False)
+
+    def test_9(self):
+        self.assertEqual(lic_9(PARAMETERS.C_PTS, PARAMETERS.D_PTS), False)
+
+    def test_10(self):
+        self.assertEqual(lic_10(PARAMETERS.E_PTS, PARAMETERS.F_PTS), False)
+
+    def test_11(self):
+        self.assertEqual(lic_11(PARAMETERS.G_PTS), False)
+
+    def test_12(self):
+        self.assertEqual(lic_12(PARAMETERS.LENGTH2), False)
+
+    def test_13(self):
+        self.assertEqual(lic_13(PARAMETERS.RADIUS2), False)
+
+    def test_14(self):
+        self.assertEqual(lic_14(PARAMETERS.AREA2), False)
