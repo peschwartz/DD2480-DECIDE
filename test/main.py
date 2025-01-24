a0c2e61e1f706cc7de28314c7cd48390f9e33c67
phoebe <phoebes@kth.se>
2025-01-24 11:26:07 +0100

    merge main into working test options

--- test/main.py
+++ test/main.py
@@ -1,7 +1,14 @@
 import unittest
 from testcmv import TestCMV
+from testfuv import TestFUV
 
 if __name__ == '__main__':
     test_suite = unittest.TestLoader().loadTestsFromTestCase(TestCMV)
     
     unittest.TextTestRunner(verbosity=2).run(test_suite)
+
+    test_suite2 = unittest.TestLoader().loadTestsFromTestCase(TestFUV)
+    
+    unittest.TextTestRunner(verbosity=2).run(test_suite2)
+
+    
