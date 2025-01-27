e12ff800c228daf2cdfd441629800d5bec135af3
phoebe <phoebes@kth.se>
2025-01-27 12:14:43 +0100

    PUM functions and tests for issue #5
    implemented PUM functions in the pum.py file as well as added testing
    for pum called testpum.py. closes #5
    
    Fixed the global variables file to initialize PUM and LCM matrix with
    correct references.

--- GLOBAL_VARS.py
+++ GLOBAL_VARS.py
@@ -6,9 +6,9 @@ PI = 3.141592653589793
 
 # Enums
 class Connectors(Enum):
-    NOTUSED = 777
-    ORR = 778
-    ANDD = 779
+    NOTUSED = 0
+    ORR = 1
+    ANDD = 2
 
 class CompType(Enum):
     LT = 1111
@@ -24,33 +24,33 @@ Vector = List[bool]  # Array of 15 booleans
 # Parameters structure
 class Parameters:
     def __init__(self):
-        self.LENGTH1: float = 0  # Length in LICs 0, 7, 12
+        self.LENGTH1: float = 3.0  # Length in LICs 0, 7, 12
         self.RADIUS1: float = 2.0  # Radius in LICs 1, 8, 13
-        self.EPSILON: float = 0  # Deviation from PI in LICs 2, 9
-        self.AREA1: float = 10    # Area in LICs 3, 10, 14
-        self.Q_PTS: int = 0      # No. of consecutive points in LIC 4
-        self.QUADS: int = 0      # No. of quadrants in LIC 4
-        self.DIST: float = 0     # Distance in LIC 6
-        self.N_PTS: int = 0      # No. of consecutive pts. in LIC 6
-        self.K_PTS: int = 0      # No. of int. pts. in LICs 7, 12
+        self.EPSILON: float = 0.2  # Deviation from PI in LICs 2, 9
+        self.AREA1: float = 10.0    # Area in LICs 3, 10, 14
+        self.Q_PTS: int = 1      # No. of consecutive points in LIC 4
+        self.QUADS: int = 4      # No. of quadrants in LIC 4
+        self.DIST: float = 5     # Distance in LIC 6
+        self.N_PTS: int = 1      # No. of consecutive pts. in LIC 6
+        self.K_PTS: int = 1      # No. of int. pts. in LICs 7, 12
         self.A_PTS: int = 1      # No. of int. pts. in LICs 8, 13
         self.B_PTS: int = 1      # No. of int. pts. in LICs 8, 13
-        self.C_PTS: int = 0      # No. of int. pts. in LIC 9
-        self.D_PTS: int = 0      # No. of int. pts. in LIC 9
-        self.E_PTS: int = 0      # No. of int. pts. in LICs 10, 14
-        self.F_PTS: int = 0      # No. of int. pts. in LICs 10, 14
-        self.G_PTS: int = 0      # No. of int. pts. in LIC 11
-        self.LENGTH2: float = 0  # Maximum length in LIC 12
-        self.RADIUS2: float = 0  # Maximum radius in LIC 13
-        self.AREA2: float = 0    # Maximum area in LIC 14
+        self.C_PTS: int = 1      # No. of int. pts. in LIC 9
+        self.D_PTS: int = 1      # No. of int. pts. in LIC 9
+        self.E_PTS: int = 1      # No. of int. pts. in LICs 10, 14
+        self.F_PTS: int = 1      # No. of int. pts. in LICs 10, 14
+        self.G_PTS: int = 1      # No. of int. pts. in LIC 11
+        self.LENGTH2: float = 4.0  # Maximum length in LIC 12
+        self.RADIUS2: float = 4.0  # Maximum radius in LIC 13
+        self.AREA2: float = 5.0    # Maximum area in LIC 14
 
 # Global variables
 PARAMETERS: Parameters = Parameters()
 POINTS: List[Coordinate] = [(1,2), (1,4), (2,2), (2,4), (6,3)]      # Array of the coordinates of data points
 NUMPOINTS: int = 5                  # Number of data points
-LCM: CMatrix = [[None] * 15] * 15   # Logical Connector Matrix
+LCM: CMatrix = [[None for _ in range(15)] for _ in range(15)]   # Logical Connector Matrix
 PUV: Vector = [None] * 15           # Preliminary Unlocking Vector
-PUM: BMatrix = [[None] * 15] * 15   # Preliminary Unlocking Matrix
+PUM: BMatrix = [[None for _ in range(15)] for _ in range(15)] # Preliminary Unlocking Matrix
 CMV: Vector = [False] * 15           # Conditions Met Vector
 FUV: Vector = [None] * 15           # Final Unlocking Vector
 LAUNCH: bool = False                # Decision: Launch or No Launch
